f733f281ba5cf83386bc6ff56bb3644fdf335654
Linda Lim <CookiesNCream@users.noreply.github.com>
2017-03-21 21:04:00 +0800

    Create insert-a-node-at-a-specific-position-in-a-linked-list.cpp

---
+++ insert-a-node-at-a-specific-position-in-a-linked-list.cpp
@@ -0,0 +1,36 @@
+/*
+  Insert Node at a given position in a linked list 
+  head can be NULL 
+  First element in the linked list is at position 0
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node * next;
+  }
+*/
+ Node * InsertNth(Node * head, int data, int position)  
+ {  
+   Node * cur = new Node,* temp = new Node;  
+   temp->data = data;  
+   temp->next = NULL;  
+   if(head == NULL){  
+     head = temp;  
+   }else{  
+     int i=0;  
+     cur = head;  
+     while(cur!=NULL){  
+       if(position == 0){  
+           temp->next = cur;  
+           head= temp;  
+       }  
+       else if(i==position-1){  
+         temp->next = cur->next;  
+         cur ->next = temp;  
+       }else{  
+       }i++;  
+       cur= cur->next;  
+     }  
+   }  
+   return head;  
+ }
